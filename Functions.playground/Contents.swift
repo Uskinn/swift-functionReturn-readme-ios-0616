1b0149cf70959dc57ab3876c638ed77b8eacff5c
Uskinn <s.nevzor@yahoo.com>
2016-07-05 16:12:27 -0400

    Done

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,45 +1,106 @@
-func printAge(name: String) {
-    print("\(name) is 29")
-}
 
-printAge("Adam")
-
-
-func happyBirthday(age: Int) {
-    print("Happy birthday #\(age)!")
-}
-
-
-func getAge(name: String) -> Int {
-    return 29
-}
-
-// Uncomment to see the error
-/*
-func badGetAge(name: String) -> Int {
-    print("\(name) is 29")
-}
-*/
-
-
-let friend = "Emily"
-var friendAge = getAge(friend)
-happyBirthday(friendAge)
-
-
-func birthdayGreeting(age: Int) -> String {
-    return "Happy birthday #\(age)!"
-}
-
-var greeting = birthdayGreeting(30)
-print(greeting)
-
+//func printAge(name: String) {
+//    
+//    print("\(name) is 29")
+//}
+//
+//func happyBirthday(age: Int) {
+//    
+//    print("Happy birthday \(age)")
+//}
+//
+//func getAge(name: String) -> Int {
+//    
+//    return 29
+//}
+//
+//let friend = "Emily"
+//var friendAge = getAge(friend)
+//
+//happyBirthday(friendAge)
+//
+//
+//func birthdayGreeting(age: Int) -> String {
+//    
+//    return "Happy birthday \(age)!"
+//}
+//
+//birthdayGreeting(30)
 
 func getAgeAndCongratulate(name: String) -> Int {
+    
     let age = 29
     print("Happy \(age)th birthday, \(name)!")
     return age
 }
 
-let friend2 = "Pete"
-var friend2Age = getAgeAndCongratulate(friend2)
+getAgeAndCongratulate("Mary")
+
+
+
+let name2 = "Mark"
+var friendAge2 = getAgeAndCongratulate(name2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//func printAge(name: String) {
+//    print("\(name) is 29")
+//}
+//
+//printAge("Adam")
+//
+//
+//func happyBirthday(age: Int) {
+//    print("Happy birthday #\(age)!")
+//}
+//
+//
+//func getAge(name: String) -> Int {
+//    return 29
+//}
+//
+//// Uncomment to see the error
+///*
+//func badGetAge(name: String) -> Int {
+//    print("\(name) is 29")
+//}
+//*/
+//
+//
+//let friend = "Emily"
+//var friendAge = getAge(friend)
+//happyBirthday(friendAge)
+//
+//
+//func birthdayGreeting(age: Int) -> String {
+//    return "Happy birthday #\(age)!"
+//}
+//
+//var greeting = birthdayGreeting(30)
+//print(greeting)
+//
+//
+//func getAgeAndCongratulate(name: String) -> Int {
+//    let age = 29
+//    print("Happy \(age)th birthday, \(name)!")
+//    return age
+//}
+//
+//let friend2 = "Pete"
+//var friend2Age = getAgeAndCongratulate(friend2)
